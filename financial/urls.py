6eb72aef931d9f92a03c0e4068c22bd0830e4fb8
zhangcheng007 <zhang44540@163.com>
2015-10-24 14:42:19 +0800

    汇率计算

---
+++ financial/urls.py
@@ -0,0 +1,12 @@
+
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+
+    #url(r'^$', 'financial.views.customer_login'),
+    #url(r'^check/','financial.views.check_login'),
+    #url(r'^logout/','customer.views.customer_logout'),
+
+
+)
+
